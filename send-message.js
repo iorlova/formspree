5584e74798cdaeacb21958bd7537dd3747159d63
iorlova <elis.orlova@gmail.com>
2016-04-02 13:08:59 +0000

    js

---
+++ send-message.js
@@ -0,0 +1,18 @@
+// JavaScript File
+
+
+$('#btn-send').click(function() {
+  
+  $.ajax({
+      url: "https://formspree.io/elis.orlova@gmail.com",  
+      method: "POST",
+      data: {
+             name: name.value,
+             email: email.value,
+             message: msg.value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#container').html('<h1>Thank you!</h1>')
+  } );
+});
